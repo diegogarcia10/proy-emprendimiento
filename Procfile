3becc72d5988da975512ff00cb1459c6effd4131
diegogarcia10 <ddjochoa.20@gmail.com>
2018-11-07 08:44:20 -0600

    Deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mercaempleado.wsgi --log-file -
--- mercaempleado/settings.py
+++ mercaempleado/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ieu$ee4_&c9j7+*h-@eaei5he%+&=n-pg91w&905wx0yhkhvz8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
