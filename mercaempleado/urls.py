d110623308701221f31f26dc73b456d6fc155ce2
fcamposp <fabrycio7campos7@gmail.com>
2018-11-15 18:37:47 -0600

    arreglos busqueda

--- mercaempleado/urls.py
+++ mercaempleado/urls.py
@@ -18,6 +18,8 @@ from django.conf.urls import url, include
 from django.urls import path
 from django.contrib.auth.views import login, logout_then_login
 from .views import home, index
+from mercaempleado.views import *
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,8 @@ urlpatterns = [
     url(r'^home/', home, name='home'),
     url(r'^accounts/login/', login, {'template_name':'autentificacion/login.html'}, name='login'),
     url(r'^logout/',logout_then_login, name='logout'),
+
+    url(r'^buscar/',busqueda, name='buscar-empleado'),
+
+    url(r'^categorias/$',categorias,name="categorias"),
 ]
