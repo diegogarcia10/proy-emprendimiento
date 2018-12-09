7a1a37dcf71859f1c13e76ff2b27351aeaeb7d05
Pablo <dc16009@ues.edu.sv>
2018-12-09 08:08:41 -0600

    2018-12-09 08:08

--- apps/empleos/models.py
+++ apps/empleos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Empleado(models.Model):
+	codigo_empleado = models.CharField(max_length = 20, primary_key = True)
+	nombre_empleado = models.CharField(max_length= 50, null = False)
+	fecha_nacimiento = models.DateField()
+	tipo_empleado = models.CharField(max_length = 50, null = False)
+	puntuacion = models.FloatField(default = 0.00)
+
 # Create your models here.
